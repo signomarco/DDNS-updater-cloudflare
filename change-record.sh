4dce70e393e52069a038076894e043e494a36877
Marco Signoretto <signomarco2003@gmail.com>
2024-03-08 22:22:03 +0100

    CODE REFACTORING AND UPDATE EVERY RECORD A
    In this commit, the script has been refactored into functions for reusability and modularity. Additionally, the functionality to update all A records associated with the specified Zone Identifier has been added.
    
    Changes Made:
    Refactored script into functions for better organization and reusability:
    check_env: Checks if the required environment variables (API_TOKEN and ZONE_IDENTIFIER) are set.
    get_public_ip: Retrieves the current public IP address of the machine.
    get_current_record: Retrieves the current DNS record from Cloudflare.
    check_old_ip: Compares the current public IP with the DNS record's IP.
    update_record: Updates all A records associated with the specified Zone Identifier if the IP addresses are different.
    Added the ability to update all A records associated with the specified Zone Identifier:
    The script now retrieves all A records and updates each one with the new public IP address if it has changed.
    Introduced color-coded output for better visibility:
    Green messages indicate successful operations.
    Red messages indicate errors or warnings.
    Added checks in update_record to handle cases where a record update fails:
    If a record update fails, an error message is displayed indicating which record could not be updated.

--- change-record.sh
+++ change-record.sh
@@ -5,41 +5,83 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-# Get the current machine public IP address
-echo "Getting the public IP address..."
-PUBLIC_IP=$(curl -s "https://ipinfo.io/ip" || curl -s "https://ipv4.icanhazip.com")
+# Load environment variables
+# Only for local development
+# for x in $(cat .env); do
+#     export $x
+# done
 
-if [ -z "${PUBLIC_IP}" ]; then
-    echo -e "${RED}The public IP address could not be retrieved${NC}"
-    exit 1
-fi
-echo -e "${GREEN}The public IP address is ${PUBLIC_IP}${NC}"
+# Check environment variables
+check_env() {
+    if [ -z "${API_TOKEN}" ]; then
+        echo "${RED}The API_TOKEN environment variable is not set${NC}"
+        local pass=0
+    fi
+
+    if [ -z "${ZONE_IDENTIFIER}" ]; then
+        echo "${RED}The ZONE_IDENTIFIER environment variable is not set${NC}"
+        local pass=0
+    fi
+
+    if [ $pass -eq 0 ]; then
+        echo "${RED}Set the environments variables${NC}"
+        exit 1
+    fi
+}
+
+# Get the current machine public IP address
+get_public_ip() {
+    echo "Getting the public IP address..."
+    PUBLIC_IP=$(curl -s "https://ipinfo.io/ip" || curl -s "https://ipv4.icanhazip.com")
+
+    if [ -z "${PUBLIC_IP}" ]; then
+        echo "${RED}The public IP address could not be retrieved${NC}"
+        exit 1
+    fi
+
+    echo "${GREEN}The public IP address is ${PUBLIC_IP}${NC}"
+}
 
 # Get the current record
-echo "Getting the current record..."
-RECORD=$(curl -s -H "Authorization: Bearer ${API_TOKEN}" "https://api.cloudflare.com/client/v4/zones/${ZONE_IDENTIFIER}/dns_records/?type=A" | jq .result)
+get_current_record() {
+    echo "Getting the current record..."
+    RECORD=$(curl -s -H "Authorization: Bearer ${API_TOKEN}" "https://api.cloudflare.com/client/v4/zones/${ZONE_IDENTIFIER}/dns_records/?type=A" | jq .result)
 
-if [ -z "${RECORD}" ]; then
-    echo -e "${RED}The record could not be retrieved${NC}"
-    exit 1
-fi
-RECORD_IP=$(echo ${RECORD} | jq '.[0].content' | tr -d '"')
-echo -e "${GREEN}The record IP address is ${RECORD_IP}${NC}"
+    if [ -z "${RECORD}" ]; then
+        echo -e "${RED}The record could not be retrieved${NC}"
+        exit 1
+    fi
+
+    RECORD_IP=$(echo ${RECORD} | jq '.[0].content' | tr -d '"')
+    echo "${GREEN}The record IP address is ${RECORD_IP}${NC}"
+}
 
 # Check if the current public IP address is the same as the record IP address
-if [ $RECORD_IP = $PUBLIC_IP ]; then
-    echo "The public IP address is the same as the record IP address, quitting..."
-    exit 0
-fi
+check_old_ip() {
+    if [ $RECORD_IP = $PUBLIC_IP ]; then
+        echo "The public IP address is the same as the record IP address, quitting..."
+        exit 0
+    fi
+}
 
 # Update the record
-echo "The record IP address is different from the public IP address. Updating the record..."
-RECORD_ID=$(echo ${RECORD} | jq '.[0].id' | tr -d '"')
-RESPONSE=$(curl -s -X PATCH -d "{\"content\":\"${PUBLIC_IP}\"}" -H "Authorization: Bearer ${API_TOKEN}" "https://api.cloudflare.com/client/v4/zones/${ZONE_IDENTIFIER}/dns_records/${RECORD_ID}")
+update_record() {
+    echo "The record IP address is different from the public IP address. Updating the record..."
+    RECORD_IDS=$(echo ${RECORD} | jq '.[].id' | tr -d '"')
+    
+    for RECORD_ID in $RECORD_IDS; do
+        RESPONSE=$(curl -s -X PATCH -d "{\"content\":\"${PUBLIC_IP}\"}" -H "Authorization: Bearer ${API_TOKEN}" "https://api.cloudflare.com/client/v4/zones/${ZONE_IDENTIFIER}/dns_records/${RECORD_ID}")
 
-if [ -z "$RESPONSE" ]; then
-    echo -e "${RED}The record could not be updated${NC}"
-    exit 1
-fi
+        if [ -z "$RESPONSE" ]; then
+            echo "${RED}The record ${RECORD_ID} could not be updated${NC}"
+        fi
 
-echo -e "${GREEN}The record has been updated with $(echo ${RESPONSE} | jq .result.content)${NC}"
+        echo "${GREEN}The records ${RECORD_ID} has been updated with $(echo ${RESPONSE} | jq .result.content)${NC}"
+    done
+}
+
+check_env
+get_public_ip
+get_current_record
+check_old_ip
+update_record
